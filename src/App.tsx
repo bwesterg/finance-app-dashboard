ac0dc13ffee307d272e4b75445065acd2fdc6290
bwesterg <benton.westergaard@gmail.com>
2024-05-07 17:57:31 -0600

    adds basic grid and components

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { createTheme } from "@mui/material/styles"
 import { useMemo } from "react"
 import { themeSettings } from "./theme"
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "@scenes/navbar";
+import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>dashboard page</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route path="/predictions" element={<div>predictions page</div>} />
             </Routes>
           </Box>
